e2e271c6ed645c3be3f4944f7554098ac00bb0d7
CHAD <chatprat22@gmail.com>
2026-02-15 21:51:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ArrayList<Integer> spirallyTraverse(int[][] arr) {
+        // code here
+        int m=arr.length;
+        int n=arr[0].length;
+        ArrayList<Integer> ans = new ArrayList<>();;
+        int minr=0;
+        int maxr=m-1;
+        int minc=0;
+        int maxc=n-1;
+        while(minr<=maxr && minc<=maxc){
+            for(int j=minr;j<=maxc;j++) ans.add(arr[minr][j]);
+           if(minr>maxr || minc>maxc) break;
+           minr++;
+           for(int i=minr;i<=maxr;i++) ans.add(arr[i][maxc]);
+           if(minr>maxr || minc>maxc) break;
+           maxc--;
+           for(int j=maxc;j>=minc;j--) ans.add(arr[maxr][j]);
+           if(minr>maxr || minc>maxc) break;
+           maxr--;
+           for(int i=maxr;i>=minr;i--) ans.add(arr[i][minc]);
+           if(minr>maxr || minc>maxc) break;
+           minc++; 
+    }
+    return ans;
+    }
+}
